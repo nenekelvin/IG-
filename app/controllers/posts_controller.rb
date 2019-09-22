671ee590a0295e24744cb5cd9f348415e0cabad3
nenekelvin <karvinmitchar@gmail.com>
2019-09-22 18:11:58 +0300

    set route path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,59 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(current_user.id)
+    @post = @user.posts.new(post_params)
+    @post.likes.build()
+    
+    respond_to do |format|
+      if @post.save
+        format.json { render :show, status: :created }
+      else
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def add_like
+    @post = Post.find(params[:post_id])
+    if @post
+      @post.likes[0].like_count +=1
+
+      if @post.likes[0].save
+        respond_to do |format|
+          format.json { render :show, status: :ok }
+        end
+      end
+    end
+  end
+
+  def add_like
+    @post = Post.find(params[:post_id])
+    if @post
+      @post.likes[0].like_count +=1
+
+      if @post.likes[0].save
+        respond_to do |format|
+          format.json { render :show, status: :ok }
+        end
+      end
+    end
+  end
+
+  private
+    def post_params
+      params.permit(:caption, :image)
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+  post '/likes/:post_id', to: 'posts#add_like', as: "add_likes"
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
 end
